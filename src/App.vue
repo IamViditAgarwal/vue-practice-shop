a32cf5e38ab64027a2ee6571965a699e854094c2
viditagarwal <vidit@elucidata.io>
2019-03-22 14:52:59 +0530

    Added the item list Added Add to Cart button Added Pay Now Button Added Remove from Cart

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-7">
+        <div class="row">
+          <div :key="product.id" class="col-md-6" v-for="product in products">
+            <Product :isInCart="isInCart(product)" v-on:add-to-cart="addToCart(product)" :product="product" />
+          </div>
+        </div>
+      </div>
+      <div class="col-md-5 my-5">
+        <Cart v-on:pay="pay()" v-on:remove-from-cart="removeFromCart($event)" :items="cart"></Cart>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import products from '@/products.json';
+import Cart from '@/components/Cart.vue';
+import Product from '@/components/Product.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components : {
+    Product,
+    Cart
+  },
+  data() {
+    return {
+      products,
+      cart: []
+    }
+  },
+  methods: {
+    addToCart: function(product) {
+      console.log(product);
+      this.cart.push(product);
+    },
+    isInCart: function(product) {
+      const item = this.cart.find(item => item.id === product.id);
+      if(item) {
+        return true;
+      }
+    },
+    removeFromCart: function(product) {
+      this.cart = this.cart.filter(item => item.id !== product.id)
+    },
+    pay: function() {
+      this.cart = [];
+      window.alert('Shopping Completed');
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color: #FBF8F3;
+  }
 </style>
---
+++ src/components/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li class="list-group-item" v-for="item in items" :key="item.id">
+                {{ item.title }} - $ {{ item.price }}
+                <button v-on:click="$emit('remove-from-cart', item)" class="btn badge badge-danger float-right">
+                    Remove from cart
+                </button>
+            </li>
+        </ul>
+        <div class="card p-3 my-5">
+            <h4 class="text-center">
+                $ {{ total }}
+            </h4>
+        </div>
+        <button :disabled="items.length === 0" v-on:click="$emit('pay')" class="btn btn-info form-control">
+            Pay Now
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Cart',
+    props: ['items'],
+    computed: {
+        total() {
+            return this.items.reduce((acc, item) => acc + Number(item.price), 0);
+        }
+    }
+}
+</script>
+
